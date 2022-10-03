fe4183908af43352c2041c70a659bc584dc5dfd0
kevinand11 <kevinfizu@gmail.com>
2022-10-03 23:23:43 +0100

    refactor: only use cloud storage for prod

--- services/api/src/modules/storage/index.ts
+++ services/api/src/modules/storage/index.ts
@@ -1,8 +1,8 @@
 import { CloudUploaderRepository } from './data/repositories/cloudUploader'
 import { LocalUploaderRepository } from './data/repositories/localUploader'
 import { StorageUseCase } from './domain/usecases/storage'
-import { isDev } from '@utils/environment'
+import { isProd } from '@utils/environment'
 
-const uploaderRepository = isDev ? new LocalUploaderRepository() : new CloudUploaderRepository()
+const uploaderRepository = isProd ? new CloudUploaderRepository() : new LocalUploaderRepository()
 
 export const StorageUseCases = new StorageUseCase(uploaderRepository)

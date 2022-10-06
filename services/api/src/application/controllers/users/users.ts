c3bd765662135a0cb3e32477737f86eaf5154f4b
kevinand11 <kevinfizu@gmail.com>
2022-10-06 03:05:24 +0100

    feat: detach card on delete

--- services/api/src/application/controllers/users/users.ts
+++ services/api/src/application/controllers/users/users.ts
@@ -1,5 +1,5 @@
 import { UsersUseCases } from '@modules/users'
-import { NotFoundError, QueryParams, Request } from '@stranerd/api-commons'
+import { QueryParams, Request } from '@stranerd/api-commons'
 
 export class UsersController {
 	static async getUsers (req: Request) {
@@ -8,8 +8,6 @@ export class UsersController {
 	}
 
 	static async findUser (req: Request) {
-		const user = await UsersUseCases.find(req.params.id)
-		if (!user) throw new NotFoundError()
-		return user
+		return await UsersUseCases.find(req.params.id)
 	}
 }

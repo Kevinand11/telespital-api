4e317f9a9393df0f9072b909ea6ed72ca2452583
kevinand11 <kevinfizu@gmail.com>
2022-10-14 14:32:43 +0100

    feat: refund session

--- services/api/src/utils/changeStreams/sessions/sessions.ts
+++ services/api/src/utils/changeStreams/sessions/sessions.ts
@@ -1,6 +1,7 @@
 import { ChangeStreamCallbacks } from '@stranerd/api-commons'
 import { SessionEntity, SessionFromModel } from '@modules/sessions'
 import { getSocketEmitter } from '@index'
+import { TransactionStatus, TransactionsUseCases, TransactionType } from '@modules/payment'
 
 export const SessionChangeStreamCallbacks: ChangeStreamCallbacks<SessionFromModel, SessionEntity> = {
 	created: async ({ after }) => {
@@ -11,13 +12,23 @@ export const SessionChangeStreamCallbacks: ChangeStreamCallbacks<SessionFromMode
 			})
 		)
 	},
-	updated: async ({ after }) => {
+	updated: async ({ after, changes }) => {
 		await Promise.all(
 			after.getParticipants().map(async (id) => {
 				await getSocketEmitter().emitUpdated(`sessions/sessions/${id}`, after)
 				await getSocketEmitter().emitUpdated(`sessions/sessions/${id}/${after.id}`, after)
 			})
 		)
+
+		if (changes.cancelled && after.cancelled && after.paid) {
+			await TransactionsUseCases.create({
+				userId: after.patient.id, email: after.patient.bio.email,
+				title: `Refund for Session: ${after.id}`,
+				amount: after.price, currency: after.currency,
+				status: TransactionStatus.fulfilled,
+				data: { type: TransactionType.RefundSession, sessionId: after.id }
+			})
+		}
 	},
 	deleted: async ({ before }) => {
 		await Promise.all(
